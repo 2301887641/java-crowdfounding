4bec17d500e0e0bbdbed776689d8e8c831078d93
sui <2301887641@qq.com>
2019-02-25 22:25:38 +0800

    设置根路径

--- crowdfunding-main/src/main/java/com/crowdfunding/listener/ContextPathListener.java
+++ crowdfunding-main/src/main/java/com/crowdfunding/listener/ContextPathListener.java
@@ -1,7 +1,25 @@
 package com.crowdfunding.listener;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
 /**
+ * 监听容器根路径
+ *
  * @author suiguozhen on 19-2-25 下午10:14
  */
-public class ContextPathListener {
+public class ContextPathListener implements ServletContextListener {
+    private static final String APP_PATH = "base";
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        ServletContext servletContext = servletContextEvent.getServletContext();
+        servletContext.setAttribute(APP_PATH, servletContext.getContextPath());
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+    }
 }
